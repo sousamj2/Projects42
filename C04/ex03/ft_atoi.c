168488bc7fb793eff17b2ad505f5776219f341c8
Mário LEGION <mdacunha@cern.ch>
2026-08-04 23:43:02 +0100

    Fixed atoi but still need to consider the under/upper limits of int.

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -9,6 +9,7 @@
 /*   Updated: 2026/08/04 15:37:47 by marisous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <stdio.h>
 
 int		ft_atoi(char *str);
 char	*ft_signcheck(char *str, int *signal);
@@ -24,7 +25,9 @@ int	ft_get_power_base(int digits, int base)
 	while (index < digits)
 	{
 		power *= base;
+		index++;
 	}
+	return (power/base);
 }
 
 char	*ft_signcheck(char *str, int *signal)
@@ -50,15 +53,15 @@ int	ft_atoi(char *str)
 	value = 0;
 	while (*str == ' ' || *str == '\t' || *str == '\n')
 		str++;
-	str = ft_signcheck(str, &index);
-	while (*str != '\0' || *str >= '0' && *str <= '9')
+	str = ft_signcheck(str, &signal);
+	while (*str != '\0' && (*str >= '0' && *str <= '9'))
 	{
 		digits++;
 		str++;
 	}
 	power = ft_get_power_base(digits, 10);
 	str -= digits;
-	while (*str != '\0' || *str >= '0' && *str <= '9')
+	while (*str != '\0' && (*str >= '0' && *str <= '9'))
 	{
 		value += ((int)(*str - '0')) * power;
 		power /= 10;
@@ -66,3 +69,7 @@ int	ft_atoi(char *str)
 	}
 	return (signal * value);
 }
+
+/* int main(){ */
+/* 	printf("Number 42 is [%d]\n", ft_atoi("42")); */
+/* } */
