f02ec28f8d22bf8a11a114b5f86634c26d8984ec
Mário LEGION <mdacunha@cern.ch>
2026-08-04 22:46:02 +0100

    Re-implementation of function to add lenght of src with truncated size of dest.

--- C03/ex05/ft_strlcat.c
+++ C03/ex05/ft_strlcat.c
@@ -15,62 +15,71 @@ int				ft_strlen(char *str);
 
 int	ft_strlen(char *str)
 {
-	char	*c;
-	int		index;
+	char	*index_char;
+	int		count_chars;
 
-	c = str;
-	index = 0;
-	while (*str != '\0')
+	index_char = str;
+	count_chars = 0;
+	while (*index_char > 0)
 	{
-		str++;
-		index++;
+		count_chars++;
+		index_char++;
 	}
-	return (index);
+	return (count_chars);
 }
 
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
 	unsigned int	index;
-	int				size_dest;
 	unsigned int	retval;
 
 	index = 0;
-	size_dest = ft_strlen(dest);
-	retval = size_dest + ft_strlen(src);
-	while (index + 1 < size && *src != '\0')
+	retval = ft_strlen(src);
+	while (*dest != '\0')
 	{
-		dest[index + size_dest] = *src;
+		if (index == size)
+			return (retval);
 		index++;
-		src++;
+		dest++;
+		retval++;
 	}
-	dest[index] = '\0';
+	while (*src != '\0')
+	{
+		if (index +1 < size)
+			*dest = *src;
+		src++;
+		dest++;
+		index++;
+	}
+	*dest = '\0';
 	return (retval);
 }
 
-/* #include <stdio.h> */
-/* #include <string.h> */
-/* #include <bsd/bsd.h> */
-/* /\* requires additional flag: -lbsd in order to work *\/ */
-/* int	main(void) */
-/* { */
-/* 	char	*src_1 = "hello"; */
-/* 	char	dest_1[] = "tes"; */
-/* 	unsigned int n; */
-/* 	unsigned int src_size; */
-/* 	n=5; */
-/* 	/\* printf("src  is [%s]\n\n",src_1); *\/ */
+/*
+#include <stdio.h>
+#include <string.h>
+#include <bsd/bsd.h>
+// requires additional flag: -lbsd in order to work 
+int	main(void)
+{
+	char	src_1[] = "yyyy";
+	char	dest_1[20] = "x";
+	unsigned int n;
+	unsigned int src_size;
+	n=2;
 
-/* 	printf("src  is [%s]\n",src_1); */
-/* 	src_size = strlcat(dest_1,src_1,n); */
-/* 	printf("dest is [%s]\n",dest_1); */
-/* 	printf("src size is [%d] (strlcat)\n\n",src_size); */
+	printf("src  is [%s]\n",src_1);
+	src_size = strlcat(dest_1,src_1,n);
+	printf("dest is [%s]\n",dest_1);
+	printf("src size is [%d] (strlcat)\n\n",src_size);
 
-/* 	char	*src_2 = "hello"; */
-/* 	char	dest_2[] = "tes"; */
-/* 	printf("src  is [%s]\n",src_2); */
-/* 	src_size = ft_strlcat(dest_2,src_2,n); */
-/* 	printf("dest is [%s]\n",dest_2); */
-/* 	printf("src size is [%d] (mine)\n",src_size); */
+	char	*src_2 = "yyyy";
+	char	dest_2[20] = "x";
+	printf("src  is [%s]\n",src_2);
+	src_size = ft_strlcat(dest_2,src_2,n);
+	printf("dest is [%s]\n",dest_2);
+	printf("src size is [%d] (mine)\n",src_size);
 
-/* 	return (0); */
-/* } */
+	return (0);
+}
+//*/
