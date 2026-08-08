365840b0aa6f0355f45e5674b2d11b4cf38f8cbe
Mario Sousa <marisous@student.42lisboa.com>
2026-08-08 09:41:52 +0100

    Solved exercise 00.

--- C05/ex00/ft_iterative_factorial.c
+++ C05/ex00/ft_iterative_factorial.c
@@ -10,4 +10,29 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+int	ft_iterative_factorial(int nb);
 
+int	ft_iterative_factorial(int nb)
+{
+	int	factorial;
+
+	factorial = 1;
+	if (nb < 0)
+		return (0);
+	if (nb < 2)
+		return (factorial);
+	while (nb > 1)
+	{
+		factorial *= nb;
+		nb--;
+	}
+	return (factorial);
+}
+
+/* #include <stdio.h> */
+/* int main(void) */
+/* { */
+/* 	int nb = 5; */
+/* 	printf("The factorial of %d is [%d]\n", nb,ft_iterative_factorial(nb)); */
+/* 	return (0); */
+/* } */
