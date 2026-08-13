a50668bd9e81ba93fde27268a761479f3630b331
Mario Sousa <marisous@student.42lisboa.com>
2026-08-13 20:18:11 +0100

    Finish C07

--- C07/ex05/ft_split.c
+++ C07/ex05/ft_split.c
@@ -10,19 +10,44 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include <stdlib.h>
+
 char	**ft_split(char *str, char *charset);
-int		ft_get_num_strings(char *str, char charset);
+void	ft_get_num_strings(char *str, char charset, int *params);
 char	*ft_simplify(char *str, char *charset);
 int		ft_strlen(char *str);
-char	*ft_get_next(char *str, char c, int *pos);
+char	*ft_get_next(char *str, char c, int *params);
+
+/*
+#define MAIN
+#ifdef MAIN
+#include <stdio.h>
+int	main(void)
+{
+	char	*str = ",.another word. next, fim...test";
+	char	*charset = ".,";
+	char	**result;
+	char	*istring;
+	result = ft_split(str, charset);
+	while (1)
+	{
+		istring = *result;
+		if (istring == NULL)
+			break ;
+		printf("Size of string [%s] is [%d]\n",istring, ft_strlen(istring));
+		result++;
+	}
+}
+#endif
+//*/
 
 int	ft_strlen(char *str)
 {
 	int	i;
 
 	i = 0;
+	if (! str)
+		return (0);
 	while (*str != '\0')
 	{
 		i++;
@@ -33,12 +58,16 @@ int	ft_strlen(char *str)
 
 char	*ft_simplify(char *str, char *charset)
 {
-	char *begset;
-	char *simple;
-	int size;
+	char	*begset;
+	char	*simple;
+	int		size;
 
 	size = ft_strlen(str);
-	simple = malloc((1+size)*sizeof(char));
+	if (!size)
+		return ("\0");
+	simple = malloc((1 + size) * sizeof(char));
+	if (!simple)
+		return ("\0");
 	while (*str != '\0')
 	{
 		begset = charset;
@@ -46,120 +75,114 @@ char	*ft_simplify(char *str, char *charset)
 		while (*begset != '\0')
 		{
 			if (*str == *begset)
-			{
 				*simple = *charset;
-				break;
-			}
 			begset++;
 		}
 		str++;
 		simple++;
 		*simple = '\0';
 	}
-	return (simple - size);	
+	return (simple - size);
 }
 
-int		ft_get_num_strings(char *str, char c)
+/**
+   params is a array of ints:
+   params[0] = index of the string up to nstrings.
+   params[1] = index of the char in the string up to strlen.
+   params[2] = current position of the cursor in the simplified string.
+   params[3] = size of a given word in the simplified string.
+   params[4] = total number of strings.
+ */
+void	ft_get_num_strings(char *str, char c, int *params)
 {
-	int nstrings;
-	int is_new_word;
-	
-	nstrings = 1;
+	int	is_new_word;
+
+	params[4] = 1;
 	is_new_word = 0;
 	if (!str || *str == '\0')
-		return (1);
+		return ;
 	while (*str != '\0')
 	{
 		if (!is_new_word && *str != c)
 		{
 			is_new_word = 1;
-			nstrings++;
+			params[4]++;
 		}
 		else if (is_new_word && *str == c)
 			is_new_word = 0;
 		str++;
 	}
-	return (nstrings);
 }
 
-int main(void)
-{
-	char str[] = ""; //,.another word. next, fim...test";
-	char *charset = " ";
-	char ** result = ft_split(str,charset);
-	char * istring;
-	while (1)
-	{
-		istring = *result;
-		if (istring == NULL)
-			break;
-		printf("Size of string [%s] is [%d]\n",istring, ft_strlen(istring));
-		result++;
-	}
-}
-
+/**
+   params is a array of ints:
+   params[0] = index of the string up to nstrings.
+   params[1] = index of the char in the string up to strlen.
+   params[2] = current position of the cursor in the simplified string.
+   params[3] = size of a given word in the simplified string.
+   params[4] = total number of strings.
+ */
 char	**ft_split(char *str, char *charset)
 {
-	char **retval;
-	char *simple;
-	int index;
-	int nstrings;
-	int pos;
+	char	**retval;
+	char	*simple;
+	int		params[5];
+	int		it;
 
-	pos = 0;
-	simple = ft_simplify(str, charset);
-	printf("[%s]\n", simple);
-	nstrings = ft_get_num_strings(simple, charset[0]);
-	printf(" and has [%d] words.\n", nstrings);
-	retval = malloc(nstrings * sizeof (char *));
-	index = 0;
-	while (index < nstrings)
+	it = 0;
+	while (it < 5)
 	{
-		printf("Pos is [%d]\n",pos);
-		retval[index] = ft_get_next(simple, charset[0], &pos);
-		printf("[%s]\n",retval[index]);
-		index++;
+		params[it] = 0;
+		it++;
+	}
+	simple = ft_simplify(str, charset);
+	ft_get_num_strings(simple, charset[0], params);
+	retval = malloc(params[4] * sizeof (char *));
+	if (!retval)
+		return (NULL);
+	while (params[0] < params[4])
+	{
+		params[3] = 0;
+		params[1] = 0;
+		retval[params[0]] = ft_get_next(simple + params[2], charset[0], params);
+		params[0]++;
 	}
 	return (retval);
 }
 
-char * ft_get_next(char *str, char c, int *pos)
+/**
+   params is a array of ints:
+   params[0] = index of the string up to nstrings.
+   params[1] = index of the char in the string up to strlen.
+   params[2] = current position of the cursor in the simplified string.
+   params[3] = size of a given word in the simplified string.
+   params[4] = total number of strings.
+ */
+char	*ft_get_next(char *str, char c, int *params)
 {
-	int size_word;
-	int index;
-	char * this_word;
+	char	*this_word;
 
-	size_word = 0;
-	index = 0;
-	str += *pos;
-	while(*str != '\0')
+	while (*str != '\0')
 	{
-		if (size_word && *str == c)
+		if (params[3] && *str == c)
 			break ;
 		if (*str != c)
-			size_word++;
+			params[3]++;
 		str++;
-		(*pos)++;
+		params[2]++;
 	}
-	/* if (*str == '\0') */
-	/* { */
-	/* 	//this_word = NULL; */
-	/* 	return (NULL); */
-	/* } */
-	//printf("size of word = [%d]\n", size_word);
-
-	if (! size_word)
+	if (! params[3])
 		return (NULL);
-	this_word = malloc((size_word +1)*sizeof(char));
-	str -= size_word;
-
-	while (index < size_word)
+	this_word = malloc((params[3] + 1) * sizeof(char));
+	if (! this_word)
+		return (NULL);
+	str -= params[3];
+	while (params[1] < params[3])
 	{
-		this_word[index] = *str;
-		index++;
+		this_word[params[1]] = *str;
+		params[1]++;
 		str++;
 	}
-	this_word[index] = '\0';
-	//printf("this word = [%s]\n", this_word);
+	this_word[params[1]] = '\0';
 	return (this_word);
 }
