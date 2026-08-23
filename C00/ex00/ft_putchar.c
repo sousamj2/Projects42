51741d51fc42e8f10517d756d8d4e6958613ebec
Mário LEGION <mdacunha@cern.ch>
2026-08-23 22:21:05 +0100

    Test commit

--- C00/ex00/ft_putchar.c
+++ C00/ex00/ft_putchar.c
@@ -6,7 +6,7 @@
 /*   By: marisous <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/23 13:55:21 by marisous          #+#    #+#             */
-/*   Updated: 2026/08/22 19:42:56 by marisous         ###   ########.fr       */
+/*   Updated: 2026/08/23 19:00:00 by marisous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,11 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 	return ;
 }
+// test
 
 /* int main(void) */
 /* { */
 /* 	ft_putchar('A'); */
 /* 	return (0); */
 /* } */
+
